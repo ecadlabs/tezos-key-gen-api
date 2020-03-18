15a894a22b643bef91b8e1ff60828e65a2fd40da
Simon Boissonneault-Robert <simrob7000@gmail.com>
2020-03-18 20:10:22 +0000

    Add timestamp to logs

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,6 @@ const winston = require('winston');
 
 export const logger = winston.createLogger({
   transports: [
-    new winston.transports.Console(),
+    new winston.transports.Console({ timestamp: true }),
   ]
 });

46d05992baae5fe7e32183c5504de56842c9c180
Gino Imbrailo <ginoimbrailo@gmail.com>
2021-03-10 12:19:12 -0800

    chore: Added florencenet to the API requests

--- src/index.ts
+++ src/index.ts
@@ -14,33 +14,6 @@ const collectDefaultMetrics = promClient.collectDefaultMetrics;
 // const register = promClient.register;
 // collectDefaultMetrics();
 
-const ErrorStatus = [401, 403, 404, 500, 503];
-
-const middlewareLogger = (func: any) => async (req: any, res: any) => {
-    const start = process.hrtime();
-
-    await func(req, res);
-    const durationInMilliseconds = getActualRequestDurationInMilliseconds(start);
-    const status = res.statusCode;
-
-    const childLogger = logger.child({
-      method: req.method,
-      url: req.url,
-      status,
-      duration: durationInMilliseconds.toLocaleString()
-    })
-
-    ErrorStatus.includes(status)? childLogger.error(res.statusMessage): childLogger.info(res.statusMessage);
-};
-
-// helper function that will calculate the time taken to complete a request
-const getActualRequestDurationInMilliseconds = (start: [number, number]) => {
-  const NS_PER_SEC = 1e9; // convert to nanoseconds
-  const NS_TO_MS = 1e6; // convert to milliseconds
-  const diff = process.hrtime(start);
-  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
-};
-
 const app = express()
 const metrics = express()
 
@@ -51,11 +24,11 @@ app.use(promMid({
   requestDurationBuckets: [0.1, 0.5, 1, 1.5],
   metricsApp: metrics
 }))
-app.post('/:network(edonet|delphinet|falphanet)', middlewareLogger((req: any, res: any) => popKeys(req, res)))
-app.get('/:network(edonet|delphinet|falphanet)', middlewareLogger((req: any, res: any) => count(req, res)))
-app.post('/:network(edonet|delphinet|falphanet)/ephemeral', middlewareLogger((req: any, res: any) => provisionEphemeralKey(req, res)))
-app.get('/:network(edonet|delphinet|falphanet)/ephemeral/:id/keys/:key', middlewareLogger((req: any, res: any) => pk(req, res)))
-app.post('/:network(edonet|delphinet|falphanet)/ephemeral/:id/keys/:key', middlewareLogger((req: any, res: any) => sign(req, res))) 
+app.post('/:network(edonet|delphinet|carthagenet|florencenet)', (req: any, res: any) => popKeys(req, res))
+app.get('/:network(edonet|delphinet|carthagenet|florencenet)', (req: any, res: any) => count(req, res))
+app.post('/:network(edonet|delphinet|carthagenet|florencenet)/ephemeral', (req: any, res: any) => provisionEphemeralKey(req, res))
+app.get('/:network(edonet|delphinet|carthagenet|florencenet)/ephemeral/:id/keys/:key', (req: any, res: any) => pk(req, res))
+app.post('/:network(edonet|delphinet|carthagenet|florencenet)/ephemeral/:id/keys/:key', (req: any, res: any) => sign(req, res))
 
 export const client: RedisClient = redis.createClient({
   host: config.redisHost,

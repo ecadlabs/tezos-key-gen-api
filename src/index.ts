add704a8133d1b54f1260015e7962d6783b1651d
Gino Imbrailo <ginoimbrailo@gmail.com>
2021-05-17 13:26:46 -0700

    chore: Added galpha2net

--- src/index.ts
+++ src/index.ts
@@ -51,11 +51,11 @@ app.use(promMid({
   requestDurationBuckets: [0.1, 0.5, 1, 1.5],
   metricsApp: metrics
 }))
-app.post('/:network(edonet|galphanet|florencenet)', middlewareLogger((req: any, res: any) => popKeys(req, res)))
-app.get('/:network(edonet|galphanet|florencenet)', middlewareLogger((req: any, res: any) => count(req, res)))
-app.post('/:network(edonet|galphanet|florencenet)/ephemeral', middlewareLogger((req: any, res: any) => provisionEphemeralKey(req, res)))
-app.get('/:network(edonet|galphanet|florencenet)/ephemeral/:id/keys/:key', middlewareLogger((req: any, res: any) => pk(req, res)))
-app.post('/:network(edonet|galphanet|florencenet)/ephemeral/:id/keys/:key', middlewareLogger((req: any, res: any) => sign(req, res))) 
+app.post('/:network(edonet|galphanet|galpha2net|florencenet)', middlewareLogger((req: any, res: any) => popKeys(req, res)))
+app.get('/:network(edonet|galphanet|galpha2net|florencenet)', middlewareLogger((req: any, res: any) => count(req, res)))
+app.post('/:network(edonet|galphanet|galpha2net|florencenet)/ephemeral', middlewareLogger((req: any, res: any) => provisionEphemeralKey(req, res)))
+app.get('/:network(edonet|galphanet|galpha2net|florencenet)/ephemeral/:id/keys/:key', middlewareLogger((req: any, res: any) => pk(req, res)))
+app.post('/:network(edonet|galphanet|galpha2net|florencenet)/ephemeral/:id/keys/:key', middlewareLogger((req: any, res: any) => sign(req, res))) 
 
 export const client: RedisClient = redis.createClient({
   host: config.redisHost,
